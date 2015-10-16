e049c4ac61492d9643cbdf7d01ccad8ddf09dbd4
liyachuan <applesstt@gmail.com>
2015-10-16 21:25:40 +0800

    update index page style

--- public/js/home.js
+++ public/js/home.js
@@ -86,7 +86,7 @@ var Robot = (function() {
     } else {
       _insertTerminal(_getHtml(robotText, true));
     }
-    
+
     if(robotImg !== '') {
       _insertTerminal(_getHtml('<img src="' + robotImg + '" />', true));
     }
