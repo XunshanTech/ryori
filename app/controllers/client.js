a7fcb12e8e97403f36dc3e0a0d4fa2a5d4163542
liyachuan <applesstt@gmail.com>
2015-12-24 22:54:51 +0800

    add code param for get access token

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -13,9 +13,10 @@ var OAuth = require('wechat-oauth');
 var client = new OAuth('wxd8cbe99c62f3c75d', 'ef485616bc8b555057109dd143d7115d');
 
 exports.order = function(req, res) {
+  var code = req.param('code');
   //var url = client.getAuthorizeURL('http://ryoristack.com/client/order', '', 'snsapi_base');
 
-  client.getAccessToken('code', function (err, result) {
+  client.getAccessToken(code, function (err, result) {
     var accessToken = result.data.access_token;
     var openid = result.data.openid;
     res.render('client/order', {
